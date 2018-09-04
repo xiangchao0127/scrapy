24377286ae7cb2ee1bdb4d0acab39185c0423de1
xiangchao <2038802094@qq.com>
2018-09-04 16:32:11 +0800

    添加scrapy测试demo

---
+++ myscrapy/myscrapy/items.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+import scrapy
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")#设置默认编码为utf-8
+class SinanewsItem(scrapy.Item):
+    # 大类的标题和url
+    parentTitle = scrapy.Field()
+    parentUrls = scrapy.Field()
+    # 小类的标题和子url
+    subTitle = scrapy.Field()
+    subUrls = scrapy.Field()
+    # 小类目录存储路径
+    subFilename = scrapy.Field()
+    # 小类下的子链接
+    sonUrls = scrapy.Field()
+    # 文章标题和内容
+    head = scrapy.Field()
+    content = scrapy.Field()
