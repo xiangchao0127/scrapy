2cf6b0392564fe50424d0ce48ca522aaa1ddb3e5
xiangchao <2038802094@qq.com>
2018-09-10 16:31:05 +0800

    写入文件测试

--- test/writeToFile.py
+++ test/writeToFile.py
@@ -3,14 +3,16 @@ import urllib
 
 import re
 
+import time
+
 """
 测试写入文件  换行
 """
-
+timeMills = 2     #每两秒写入一行数据
 page = urllib.urlopen('https://www.baidu.com/')
 htmlcode = page.read()
 print htmlcode
-for i in range(1,30):
+for i in range(30):
     htmlcode += 'url=https://www.baidu.com/" '
 print htmlcode
 reg = r'https://www.baidu.com/'#正则表达式
@@ -24,6 +26,8 @@ for i in range(len(imglist)):
     print i
     if(i%4==0 and i!=0):
         pageFile.write("\n", )  # 写入
+        time.sleep(timeMills)
+        pageFile.flush()
     pageFile.write(imglist[i] + "     ")
 
 pageFile.close()#开了记得关
