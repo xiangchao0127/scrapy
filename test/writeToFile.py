ab7e181790504057f50658f41065057eeb33452d
xiangchao <2038802094@qq.com>
2018-09-10 16:21:36 +0800

    写入文件测试

---
+++ test/writeToFile.py
@@ -0,0 +1,29 @@
+#coding:utf-8
+import urllib
+
+import re
+
+"""
+测试写入文件  换行
+"""
+
+page = urllib.urlopen('https://www.baidu.com/')
+htmlcode = page.read()
+print htmlcode
+for i in range(1,30):
+    htmlcode += 'url=https://www.baidu.com/" '
+print htmlcode
+reg = r'https://www.baidu.com/'#正则表达式
+reg_img = re.compile(reg)#编译一下，运行更快
+imglist = reg_img.findall(htmlcode)#进行匹配
+print imglist
+
+pageFile = open('pageCode.txt','w')#以写的方式打开pageCode.txt
+
+for i in range(len(imglist)):
+    print i
+    if(i%4==0 and i!=0):
+        pageFile.write("\n", )  # 写入
+    pageFile.write(imglist[i] + "     ")
+
+pageFile.close()#开了记得关
